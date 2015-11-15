719d2d05ac536998a2e9f8faa9d140719cded8cc
zppratt <zppratt@gmail.com>
2015-11-15 15:29:08 -0500

    Initial class library created.

--- ClassLibrary/Class1.cs
+++ ClassLibrary/Class1.cs
@@ -5,7 +5,25 @@ using System.Text;
 
 namespace ClassLibrary
 {
-    public class Class1
+    public class Movie :: IMovie
     {
     }
+
+    public interface IMovie
+    {
+        int ID { get; set; }
+        string Title { get; set; }
+        string Synopsis { get; set; }
+        int Year { get; set; }
+    }
+
+    public interface IMovieRepository
+    {
+        IEnumerable<IMovie> GetAll();
+        IMovie GetByID(int id);
+        IMovie GetByTitle(string title);
+        void Add(IMovie movie);
+        void Remove(IMovie movie);
+    }
+
 }
