ddc2efe3584a21420f119c6f64eedc83879b9738
zppratt <zppratt@gmail.com>
2015-11-24 14:43:15 -0500

    Basically restarted

---
+++ ApteraMovies/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApteraMovies
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource movieViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("movieViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // movieViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource movieRepositoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("movieRepositoryViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // movieRepositoryViewSource.Source = [generic data source]
+        }
+
+        private void addMovie()
+        {
+            
+        }
+
+        private void removeMovie()
+        {
+
+        }
+
+    }
+}
---
+++ Model/Interfaces.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Movie : IMovie
+    {
+        int _id;
+        string _synopsis;
+        string _title;
+        int _year;
+        
+        public Movie()
+        {
+
+        }
+
+        public Movie(string title, string synopsis, int year)
+        {
+            this.ID = this.GetHashCode();
+            this.Synopsis = synopsis;
+            this.Title = title;
+            this.Year = year;
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Synopsis
+        {
+            get { return _synopsis; }
+            set { _synopsis = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+    }
+
+    public class MovieRepository : IMovieRepository
+    {
+        // Dictionary object to store all the movies in memory
+        Dictionary<int, IMovie> _movies = new Dictionary<int, IMovie>();
+
+        // Adds a movie, uses ID as key in the dictionary object
+        public void Add(IMovie movie)
+        {
+            try {
+                _movies.Add(movie.ID, movie);
+            }
+            catch
+            {
+                movie.ID++;
+                _movies.Add(movie.ID, movie);
+            }
+        }
+
+        // Returns the movies in the Dictionary
+        public IEnumerable<IMovie> GetAll()
+        {
+            return _movies.Values;
+        }
+
+        // Gets movies by id, throws exception if no movie is in the Dictionary
+        public IMovie GetByID(int id)
+        {
+            try {
+                return _movies[id];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        // Loop through movies and return the first match
+        public IMovie GetByTitle(string title)
+        {
+            foreach (IMovie movie in _movies.Values)
+            {
+                if (movie.Title.Equals(title))
+                {
+                    return movie;
+                }
+            }
+            return null; // no movie found
+        }
+
+        // Removes movie entry by ID
+        public void Remove(IMovie movie)
+        {
+            _movies.Remove(movie.ID);
+        }
+    }
+
+    public interface IMovie
+    {
+        int ID { get; set; }
+        string Title { get; set; }
+        string Synopsis { get; set; }
+        int Year { get; set; }
+    }
+
+    public interface IMovieRepository
+    {
+        IEnumerable<IMovie> GetAll();
+        IMovie GetByID(int id);
+        IMovie GetByTitle(string title);
+        void Add(IMovie movie);
+        void Remove(IMovie movie);
+    }
+
+}
